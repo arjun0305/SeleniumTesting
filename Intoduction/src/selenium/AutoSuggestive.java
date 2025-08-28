06866aff6b9c8664ac7cb03a90259a6770cb0538
arjunc <arjunc@amdocs.com>
2025-08-28 15:56:05 +0530

    Added static dropdown and Dynamic dropdown

---
+++ Intoduction/src/selenium/AutoSuggestive.java
@@ -0,0 +1,34 @@
+package selenium;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AutoSuggestive {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "src\\Resources\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+	
+		driver.findElement(By.id("autosuggest")).sendKeys("ind");
+		Thread.sleep(3000);
+		List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+		
+//		Thread.sleep(2000);
+		for (WebElement option : options) 
+		{
+//			System.out.println(option.getText());   //To print country names
+			if(option.getText().equalsIgnoreCase("India"))
+			{
+				option.click();
+				break;
+			}
+		}
+		
+	}
+}
--- Intoduction/src/selenium/UpdatedDropDown.java
+++ Intoduction/src/selenium/UpdatedDropDown.java
@@ -8,28 +8,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class UpdatedDropDown {
 	
 	public static void main(String[] args) throws Exception {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ARJUNC\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "src\\Resources\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		Scanner sc = new Scanner(System.in);
 		
+//		Set From city	
+		System.out.println("Enter from City code");
+		String fromCity =sc.next();
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).sendKeys(fromCity);
+		
+//		Set to City
+		System.out.println("Enter to City code");
+		String toCity = sc.next();
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='"+toCity+"']")).click();
+		
 		System.out.println("How many adults you want to add" );
 		int numberOfAdults = sc.nextInt();
-		
-		
 //		Select number of adults 
 		driver.findElement(By.id("divpaxinfo")).click();
 		Thread.sleep(2000);
-		int i=1;
-		
+//		int i=1;
 		if(numberOfAdults!=1)
 		{
-			while(i<numberOfAdults)
-//			while(numberOfAdults>1)
+//			while(i<numberOfAdults)
+			while(numberOfAdults>1)
 			{
 				driver.findElement(By.id("hrefIncAdt")).click(); //number of adults as per input
-				i++;
-//				numberOfAdults--;
+//				i++;
+				numberOfAdults--;
 			}
 			driver.findElement(By.id("btnclosepaxoption")).click();
 		}
